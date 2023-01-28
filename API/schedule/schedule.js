b32a17edf29843e2808992b09b5c0d6a91e3e612
Sharif <sharifhosen73@gmail.com>
2023-01-28 13:32:29 +0600

    clean

--- API/schedule/schedule.js
+++ API/schedule/schedule.js
@@ -6,26 +6,18 @@ const Schedule = client.db("Kalender").collection("schedule");
 // single schedule post
 const singleSchedulePost = (app) => {
   app.post("/single-schedule", async (req, res) => {
-    try {
-      const booking = req.body;
-
-      const result = await Schedule.insertOne(booking);
-
-      res.send(result);
-    } finally {
-    }
+    const booking = req.body;
+    const result = await Schedule.insertOne(booking);
+    res.send(result);
   });
 };
 
 // single schedule get
 const singleScheduleGet = (app) => {
   app.get("/single-schedule", async (req, res) => {
-    try {
-      const query = {};
-      const result = await Schedule.find(query).toArray();
-      res.send(result);
-    } finally {
-    }
+    const query = {};
+    const result = await Schedule.find(query).toArray();
+    res.send(result);
   });
 };
 
